f8236c86dc9b98666fd22e9bb38afa0dc869f435
RajatKumarA50 <53323521+RajatKumarA50@users.noreply.github.com>
2020-09-10 22:58:13 +0530

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -5,33 +5,34 @@
 public class Q4 {
 	
 	
-	    public static void main(String[] args)
-	    {
-	        int r,q=0,s=0,temp=0,large=0;
-	        for(int i=100;i<1000;i++)
-	        {
-	            
-	            for(int j=100;j<1000;j++)
-	            {
-	             temp=i*j;
-	             q=temp;
-	             s=0;
-	             while(temp>0)
-	             {
-	              r=temp%10;
-	              s=(s*10)+r;     
-	              temp=temp/10;
-	             }
-	             if(q==s&&q>=large)
-	             {
-	                 large=q;
-	                 
-	                }
+	   public static boolean isPalindrome(int nr) {
+	    int rev = 0;                    // the reversed number
+	    int x = nr;                     // store the default value (it will be changed)
+	    while (x > 0) {
+	        rev = 10 * rev + x % 10;
+	        x /= 10;
+	    }
+	    return nr == rev;               // returns true if the number is palindrome
+	}
+	public static void main(String[] args) 
+	{
+	
+		int max = -1;
+
+	    for ( int i = 999 ; i >= 100 ; i--) {
+	        if ( max >= i*999 ) { 
+	            break;
+	        }
+	        for (int j = 999 ; j >= i ; j-- ) {             
+	            int p = i * j;
+	            if ( max < p && isPalindrome(p) ) {
+	                max = p;
 	            }
 	        }
-	                System.out.println(large);
-	                
-	    }
+	    }       
+	    System.out.println(max > -1? max : "No palindrome found");
+
+}
 	}
 	            
 
